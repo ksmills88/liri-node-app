ab29176ecaa69ffb0c07a5ed7a9914aeb3f9cc01
kmills <ksmills82@gmail.com>
2018-04-12 01:29:58 -0400

    working on README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # liri-node-app
+
+LIRI is a command line node app that takes in parameters and gives you back data. 
+
+There are currently 4 commands that LIRI recognizes:
+
+my-tweets: 
+
+when called, my last 2 tweets will display with their timestamps.
+Future enhancements could include assigning the handles to a variable so that they could change in the command line.
+
+spotify-this-song:
+
+When called, this will default to a pre-assigned song and display information about the song, artist, album, and where to go to listen to a preview. We are pulling this information from the spotify-node-api.
+When called, and then followed with a song name in format: spotify-this-song "<song name>" , the information for that song name will display.
+
+movie-this:
+
+When called, this will default to a pre-assigned movie and display information about the movie from IMDB's API.
+When called, and then followed with a movie name in format: movie-this "<movie name>" , the information for that movie will display.
+
+do-what-it-says:
+
+When called, LIRI will take the text inside of a text file and use it to call on one of the 3 previous commands using the fs Node package to read the text file. 
--- liri.js
+++ liri.js
@@ -45,12 +45,111 @@ switch (userInput){
             showMovieInfo(movie);
         }
         break;
-    
+    case 'do-what-it-says':
+        doWhatItSays();
+        break;
+    default:
+        console.log("type something else, it didn't do what you thought it would");
+        break;
 }
+
+
 // Function for tweets
+function myTweets() {
+    var params = {
+        screen_name: "kookykarma",
+        count: "2"
+    };
+    // only did a count of 2 because I only have 2 tweets :)
+
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            var myTweet = JSON.parse(response.body);
+            myTweet.forEach(function(tweet) {
+                console.log(tweet.created_at);
+                console.log(tweet.text);
+            });
+        }
+        else
+        {
+            console.log(error);
+        }
+    });
+}
 
 // function for songs
+function spotifyThisSong(songName){
+    spotify.search({ 
+        type: 'track', 
+        query: songName,
+        limit: 1 
+    }, function(err, data) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+        else
+        {
+            data.tracks.items.forEach(function(event){
+                console.log("***Artist: " + event.album.artists[0].name + "***");
+                console.log(event.name);
+                console.log("Go to this site to hear a preview: " + event.preview_url);
+                console.log("Album: "+ event.album.name);
+
+            });
+        }
+     
+        // Do something with 'data' 
+    });
+}
 
 // function for movies
+function showMovieInfo(movieName){
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    request(queryUrl, function(err, response, body){
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+        else
+        {
+            var m = JSON.parse(response.body);
+            console.log("***Title: "+ m.Title + "***");
+            console.log("Release date: " + m.Year);
+            console.log("IMDB rating: "+ m.Ratings[0].Value);
+            console.log("Rotten Tomatoes rating: " + m.Ratings[1].Value);
+            console.log("Production location(s): " + m.Country);
+            console.log("Language(s): " + m.Language);
+            console.log("Plot: " + m.Plot);
+            console.log("Main Actors: "+ m.Actors);
+        }
+    })
+}
 
 // function for do what it says
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function(err, data){
+        if (err) {
+            return console.log(err);
+        }
+        var textArray = data.split(",");
+        var command = textArray[0];
+        var input = textArray[1];
+// Assuming the txt file will have accurate information, I left out the conditionals in the switch
+        switch(command){
+            case 'my-tweets':
+                myTweets();
+                break;
+            case 'spotify-this-song':
+                spotifyThisSong(input);
+                break;
+            case 'movie-this':
+                showMovieInfo(input);
+                break;
+            default:
+                console.log("try again.");
+                break;
+        }
+
+    });
+}
